4a1adaff1d19046c8ffc4335d177304cc43974d5
bac2 <bac2g10@ecs.soton.ac.uk>
2013-04-25 13:54:02 +0100

    Adjusted wifi script to control the time too

--- wifi.py
+++ wifi.py
@@ -11,9 +11,17 @@ def is_wifi_on():
 		return False
 
 if __name__ == '__main__':
+	i = 0
 	while True:
+		i +=1
 		if not is_wifi_on():
 			print 'WiFi Off - Cycling'
 			os.system('sudo /home/pi/3YP/cycle_wifi.sh')
+		else:
+			#Wifi is on
+			if i % 60 == 0:
+				i = 0
+				os.system('sudo service ntp stop')
+				os.system('sudo ntpd -qg')
 		time.sleep(60)
 
