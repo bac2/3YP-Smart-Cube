b3ba290ee407a71b176a6fc5ca39e8f9eb82a896
Ben Clive <bac2g10@ecs.soton.ac.uk>
2013-05-15 18:35:48 +0100

    Cube now adds events correctly

--- web/cube.py
+++ web/cube.py
@@ -197,5 +197,7 @@ class EventHandler(BaseHandler):
                 if friend is None:
                     #Not friends!
                     return
+
+                cube_id = self.db.query("SELECT id FROM Cube WHERE unique_id=%s", cube_code);
                 
                 self.db.execute("INSERT INTO Event (cube_id, owner, action, rotation, profile_id) VALUES (%s, %s, %s, %s, %s);", cube_id, current_user.user_id, action, side, profile_id);
